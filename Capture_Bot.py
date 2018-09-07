448be847c773a6873abad2f5afff3f1ce3122379
Naveen Konrajankuppam Mahavishnu <Naveen94@users.noreply.github.com>
2018-09-07 00:08:23 -0500

    Update Capture_Bot.py

--- Capture_Bot.py
+++ Capture_Bot.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from selenium import webdriver
 
-#Download the Selenium driver for chrome and load the appropriate path - Can also use Firefox and IE as well
+#Download the Selenium driver for chrome and load the appropriate path - Can use Firefox and IE as well
 
 driver=webdriver.Chrome(executable_path="fakepath\chromedriver.exe") 
 
@@ -12,7 +12,7 @@ driver=webdriver.Chrome(executable_path="fakepath\chromedriver.exe")
 
 param = 'https://'
 
-#list of domains such as www.sampleurl.com
+# Input csv file contains list of domains such as www.sampleurl.com, www.sampleurl2.com
 
 csv_input = open('fakepath\<filename>.csv', "rb")
 reader  = csv.reader(csv_input)
@@ -26,16 +26,17 @@ csv_input.close()
 #create a list of urls in a desired format like - https://www.sampleurl.com
 
 complete_urls = [param + client_url for client_url in urls]  
-driver.maximize_window()
 
+# resize the browser to fit the window
+driver.maximize_window()
 
 i=0
 for complete_url in complete_urls:
-        # Logic to open the browser and load URLs
+        # open chrome and load URLs
         driver.get(complete_url)
-        #Logic to take screenshot of the webpage
+        # take screenshot of the domain webpage
 	driver.save_screenshot("fakepath\%s.png" % urls[i]) 
-        #Logic to add URL name to the image
+        # append domain name to the screenshot
 	image = cv2.imread("fakepath\%s.png" % urls[i])
 	texted_image =cv2.putText(image ,text="%s" % complete_url, org=(800,200),fontFace=2, fontScale=2, color=(0,0,255), thickness=5)
         cv2.imwrite("fakepath\%s.png" % urls[i],texted_image)
